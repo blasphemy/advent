56fc6f4d24f46be55dca677e3666961ba9b57970
Daniel Leining <daniel@leining.org>
2019-11-29 08:55:01 +0000

    gofmt

--- aocommon/execute.go
+++ aocommon/execute.go
@@ -2,20 +2,20 @@ package aocommon
 
 import "time"
 
-func ExecuteDefault(year,day,part string) (int,time.Duration,error) {
-	f,err := getFunc(year,day,part)
+func ExecuteDefault(year, day, part string) (int, time.Duration, error) {
+	f, err := getFunc(year, day, part)
 	if err != nil {
 		return 0, 0, err
 	}
-	in := getInput(year,day,part)
+	in := getInput(year, day, part)
 	sTime := time.Now()
 	ans := f(in)
 	finTime := time.Since(sTime)
 	return ans, finTime, nil
 }
 
-func ExecuteInput(year,day,part,input string) (int,time.Duration,error) {
-	f, err := getFunc(year,day,part)
+func ExecuteInput(year, day, part, input string) (int, time.Duration, error) {
+	f, err := getFunc(year, day, part)
 	if err != nil {
 		return 0, 0, err
 	}
--- aocommon/registry.go
+++ aocommon/registry.go
@@ -13,27 +13,27 @@ func InitRegistry() {
 	importFuncs()
 }
 
-func registerWithInput(year,day,part int,input string, answerfunc AOCFunc) {
+func registerWithInput(year, day, part int, input string, answerfunc AOCFunc) {
 	//this one takes ints to make it easier on me
 	ys := strconv.Itoa(year)
 	ds := strconv.Itoa(day)
 	ps := strconv.Itoa(part)
-	registerAOCFunc(ys,ds,ps, answerfunc)
-	registerAOCInput(ys,ds,ps,input)
+	registerAOCFunc(ys, ds, ps, answerfunc)
+	registerAOCInput(ys, ds, ps, input)
 }
 
-func registerAOCFunc(year,day,part string, answerfunc AOCFunc) {
-	key := getKey(year,day,part)
+func registerAOCFunc(year, day, part string, answerfunc AOCFunc) {
+	key := getKey(year, day, part)
 	funcRegistry[key] = answerfunc
 }
 
-func registerAOCInput(year,day,part,input string) {
-	key := getKey(year,day,part)
+func registerAOCInput(year, day, part, input string) {
+	key := getKey(year, day, part)
 	aocInputs[key] = input
 }
 
-func getFunc(year,day,part string) (AOCFunc,error) {
-	key := getKey(year,day,part)
+func getFunc(year, day, part string) (AOCFunc, error) {
+	key := getKey(year, day, part)
 	f, ok := funcRegistry[key]
 	if !ok {
 		e := errors.New("Function Does Not Exist")
@@ -42,12 +42,12 @@ func getFunc(year,day,part string) (AOCFunc,error) {
 	return f, nil
 }
 
-func getKey(year,day,part string) string {
+func getKey(year, day, part string) string {
 	return year + "-" + day + "-" + part
 }
 
-func getInput(year,day,part string) string {
-	key := getKey(year,day,part)
+func getInput(year, day, part string) string {
+	key := getKey(year, day, part)
 	i := aocInputs[key]
 	return i
 }

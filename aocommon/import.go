1d8c26bc3a61c35f76edc389b8046876a26194d1
Daniel Leining <daniel@leining.org>
2019-11-30 00:15:23 +0000

    migrate 2017 day 1 to aocommon

--- aocommon/import.go
+++ aocommon/import.go
@@ -8,6 +8,7 @@ import (
 	"aoc2/aocommon/solutions/y2015d5"
 	"aoc2/aocommon/solutions/y2015d6"
 	"aoc2/aocommon/solutions/y2016d1"
+	"aoc2/aocommon/solutions/y2017d1"
 )
 
 func registerAll() {
@@ -18,4 +19,5 @@ func registerAll() {
 	registerSolution(y2015d5.Solution)
 	registerSolution(y2015d6.Solution)
 	registerSolution(y2016d1.Solution)
+	registerSolution(y2017d1.Solution)
 }
